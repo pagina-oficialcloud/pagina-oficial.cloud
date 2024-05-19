29860c8f513b7a55708a7f6e7bcbbc8c37a8f5b5
pagina-oficialcloud <168871450+pagina-oficialcloud@users.noreply.github.com>
2024-05-18 22:38:32 -0300

    Add files via upload

---
+++ quiz/js/hideAllExcept.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    hideAllExcept();
+});
+
+function hideAllExcept() {
+    // document.getElementById("popupContainer").style.display = "none";
+    document.getElementById("pergunta01").style.display = "block";
+    document.getElementById("pergunta02").style.display = "none";
+    document.getElementById("pergunta03").style.display = "none";
+    document.getElementById("pergunta04").style.display = "none";
+    document.getElementById("finalizacao").style.display = "none";
+}
